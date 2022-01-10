67b9f78fc1105a8c6fe0528dd8389331aca06a12
Lelabeshidze <lela.beshidze@gmail.com>
2022-01-10 17:12:42 +0400

    Update app.js

--- app.js
+++ app.js
@@ -70,14 +70,16 @@ let numbersarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 let largestN = numbersarray[0];
 let secondlargestn = numbersarray[0];
 for (let i = 0; i < numbersarray.length; i++) {
-    if (largestN < numbersarray[i] ) {
-        largestN = numbersarray[i];
+    if (numbersarray[i]>largestN  ) {
+    secondlargestn = largestN;
+        largestN = numbersarray[i];   
     } 
     else if(numbersarray[i] > secondlargestn && numbersarray[i] !=largestN ) 
         secondlargestn=numbersarray[i];
     
 }
 console.log(secondlargestn);
+
 
 
 let thirdarray = [ 2, 3, 6, 6, 5 ]
